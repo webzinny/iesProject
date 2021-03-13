19a51fbce245c91cc2bac2d49b791c0be264ac4e
webzinny <webzinny@gmail.com>
2021-03-13 18:20:01 +0530

    Models Finalized

--- ies/api/admin.py
+++ ies/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class teacherAdmin(admin.ModelAdmin):
+    list_display=['id','name','email','pas']
+
+class studentAdmin(admin.ModelAdmin):
+    list_display=['id','branch','sem','sec','enroll','name','dob','email','phone','add','pas','att']
+
+class attendanceAdmin(admin.ModelAdmin):
+    list_display=['id','date','stu']
+
+admin.site.register(teacher,teacherAdmin)
+admin.site.register(student,studentAdmin)
+admin.site.register(attendance,attendanceAdmin)
--- ies/api/models.py
+++ ies/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class teacher(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    pas=models.CharField(max_length=15)
+
+class student(models.Model):
+    branch=models.CharField(max_length=5)
+    sem=models.IntegerField()
+    sec=models.CharField(max_length=2)
+    enroll=models.CharField(max_length=10)
+    name=models.CharField(max_length=32)
+    dob=models.DateField()
+    email=models.EmailField()
+    phone=models.IntegerField()
+    add=models.CharField(max_length=64)
+    pas=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.enroll
+
+    def att(self):
+        return [i.date for i in attendance.objects.filter(enroll=self.id)]
+
+class attendance(models.Model):
+    date=models.DateField()
+    enroll=models.ManyToManyField(student)
+
+    def stu(self):
+        return [i.id for i in self.enroll.all()]
